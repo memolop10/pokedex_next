57ca05cc109e9e629cab3316c81184f2c4fac989
memolop10 <inglopezvargas@gmail.com>
2023-11-09 20:47:10 -0600

    first commit 2

--- pages/pokemon/[idPokemon].jsx
+++ pages/pokemon/[idPokemon].jsx
@@ -2,24 +2,31 @@ import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 
-const getPokemon = async() => {
-            try {
-                const res = await fetch(url);
-                const data = await res.json();
-                return data 
-            } catch (error) {
-                console.log(error)
-            } 
-}
+
 
 const PokemonDetail = () => {
+    const [pokeDetail, setPokeDetail] = useState({})
     const router = useRouter()
     console.log(router.query)
     const { url = '' } = router.query
-    const [pokeDetail, setPokeDetail] = useState({})
+
+    
 
     useEffect(() => {
-        getPokemon().then((data) => setPokeDetail(data))
+        const getPokemon = async(url) => {
+            if (url) {
+                try {
+                    const res = await fetch(url);
+                    const data = await res.json();
+                    console.log(data)
+                    setPokeDetail(data) 
+                } catch (error) {
+                    console.log(error)
+                } 
+            }
+        }
+
+        getPokemon()
     }, [url])
     
     console.log(pokeDetail)
